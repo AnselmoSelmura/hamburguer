b960b132879bea87e13d904d30f182dbbd8306f2
Anselmo Diabaca <anselmo23diababa@gmail.com>
2023-06-23 11:25:59 +0000

    fix: componente

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
+  <div class="user" v-show="showName">
+    Nome: {{ firstName }} <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div class="option">
+    <div v-if="accessLevel === Admin">User Admin</div>
+    <div v-else-if="accessLevel === Marketing">User Marketing</div>
+    <div v-else="accessLevel === User">normal User</div>
+  </div>
+
+  <cabeca v-if="showHeader"/>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import cabeca from './components/cabeca.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    cabeca
+  },
+  data(){
+    return{
+      showHeader: true,
+      firstName: 'Selmo',
+      lastName: 'Selmura',
+      showName: true,
+      accessLevel: 'Marketing',
+    }
   }
 }
 </script>
---
+++ src/components/cabeca.vue
@@ -0,0 +1,68 @@
+
+<template>
+    <div class="header">
+        <h1>Com todos Molhos</h1>
+    </div>
+
+    <div class="loop">
+        <div v-for="(obj, index) in todos"
+        v-bind:key="obj.id"
+        class="todoItem"
+        >
+       {{ index }} {{ obj.title }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'cabeca',
+        data(){
+            return{
+                todos: [
+                    {
+                        "userId":1,
+                        "id":1,
+                        "title":"Frango",
+                        "completed": false
+                    },
+                    {
+                        "userId":1,
+                        "id":2,
+                        "title":"Mix Picanha",
+                        "completed": false
+                    },
+                    {
+                        "userId":1,
+                        "id":3,
+                        "title":"Carne Bovina",
+                        "completed": false
+                    },
+                    {
+                        "userId":1,
+                        "id":4,
+                        "title":"Carne Suina",
+                        "completed": false
+                    },
+                    {
+                        "userId":1,
+                        "id":5,
+                        "title":"Carne Caprina",
+                        "completed": false
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+    .todoItem{
+        background-color: rgba(0, 42, 24, .5);
+        margin: 0 0 5px 0;
+        padding: 10px;
+    }
+    .header{
+        background-color: rgba(0, 0, 0, .5);
+    }
+</style>
